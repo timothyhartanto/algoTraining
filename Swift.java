340a948b10c9e1fecfbb1dc5c4ff108515709c11
timothyhartanto <hartantotimothy@gmail.com>
2019-06-19 21:28:56 +0700

    add notes

--- Swift.java
+++ Swift.java
@@ -1,6 +1,13 @@
 import java.util.Arrays; 
 
 public class Swift {
+  // add an additional 0 on the next index
+  // ex: [1, 0, 1, 0, 5, 3, 4]
+  // res: [1, 0, 0, 1, 0, 0, 5]
+  // the paramater array has to be modified
+  // if there are multiple 0, add additional 0 equals to number of consecutives 0
+  // ex: [1, 0, 0, 0, 5, 3, 4]
+  // res: [1, 0, 0, 0, 0, 0, 0]
   public void swift(int[] arr) {
     int[] temp = Arrays.copyOf(arr, arr.length);
     int j = 0;
