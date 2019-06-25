58f925df2914494aaeba23e745159e543c940483
timothyhartanto <hartantotimothy@gmail.com>
2019-06-26 00:03:50 +0700

    reduce time complexity

--- CalculateWays.java
+++ CalculateWays.java
@@ -1,12 +1,21 @@
 public class CalculateWays {
-  public int calculateWays(int n) {
+  public int calculateWays(int n, int[] arr) {
     if(n == 0)
       return 1;
     if(n < 0)
       return 0;
     int sum = 0;
     for(int i = 1; i <= 6; i++){
-      sum += calculateWays(n - i);
+      if(n - i >= 0 && arr[n - i] != 0){
+        sum += arr[n -i];
+      } else {
+        if (n - i >= 0) { 
+          arr[n -i] = calculateWays(n - i, arr);
+    		  sum += arr[n -i];
+        } else {
+          sum += calculateWays(n - i, arr);
+        }
+      }
     }
     return sum;
   }
